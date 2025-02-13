89598557b4c715a7597e97abf5f0bb04724fe34e
fabi <office@bucurieindar.ro>
2025-02-13 18:30:47 +0200

    migr a assa d

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,4 +1,5 @@
-import * as migration_20250211_180251_initial from './20250211_180251_initial'
+import * as migration_20250211_180251_initial from './20250211_180251_initial';
+import * as migration_20250213_162905_initial from './20250213_162905_initial';
 
 export const migrations = [
   {
@@ -6,4 +7,9 @@ export const migrations = [
     down: migration_20250211_180251_initial.down,
     name: '20250211_180251_initial',
   },
-]
+  {
+    up: migration_20250213_162905_initial.up,
+    down: migration_20250213_162905_initial.down,
+    name: '20250213_162905_initial'
+  },
+];
