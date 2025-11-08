c398f5c306fe5d753845777b7c3cf767b3ed3f5d
Fabian Ghenof <fabianghenof@techie.com>
2025-11-08 17:49:10 +0200

    no revista

--- src/utilities/commonInfo.ts
+++ src/utilities/commonInfo.ts
@@ -1,3 +1,3 @@
 //common phone number, website name, etc
-export const websiteTitle = 'Revista în Pridvor'
+export const websiteTitle = 'în Pridvor'
 export const websiteDescription = 'Unde tihna este la ea acasă'
