06585ae42357138b563492564eade2ca4b8ba844
Fabian Ghenof <fabianghenof@techie.com>
2025-11-08 17:49:28 +0200

    Update commonInfo.ts

--- src/utilities/commonInfo.ts
+++ src/utilities/commonInfo.ts
@@ -1,3 +1,3 @@
 //common phone number, website name, etc
-export const websiteTitle = 'în Pridvor'
+export const websiteTitle = 'În Pridvor'
 export const websiteDescription = 'Unde tihna este la ea acasă'
