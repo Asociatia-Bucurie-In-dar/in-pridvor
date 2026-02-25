ece9cc73fa173f9d314367e2e6a34cef6ac8c61c
Fabi <fabianghenof@techie.com>
2026-02-25 23:21:59 +0200

    Update index.tsx

--- src/components/AdminDashboard/index.tsx
+++ src/components/AdminDashboard/index.tsx
@@ -2,11 +2,7 @@ import React from 'react'
 import { CacheRevalidationWidget } from './CacheRevalidationWidget'
 
 const AdminDashboard: React.FC = () => {
-  return (
-    <div>
-      <CacheRevalidationWidget />
-    </div>
-  )
+  return <div>{/* <CacheRevalidationWidget /> */}</div>
 }
 
 export default AdminDashboard
